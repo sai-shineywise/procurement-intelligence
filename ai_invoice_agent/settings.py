81247b9fee79d77e66270fb90201505515e14228
sai-shineywise <sai.praveen@shineywise.com>
2025-02-20 15:23:15 +0530

    update allowed hosts

--- ai_invoice_agent/settings.py
+++ ai_invoice_agent/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-frlfuxw1a#cf8kf+2v()l2zd&%q9b*fmk2aq#^g$sd34mgr)w*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
